df6fb1cce8789d4e48fcd8f2cdba5fce294f3b48
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-11-24 14:47:41 +0530

    Update guvih10.py

--- guvih10.py
+++ guvih10.py
@@ -4,12 +4,12 @@ d=raw_input().split()
 a=int(a)
 b=int(b)
 e=len(d)
-flag=0
+count=0
 if(len(c)==a and len(d)==b):
-	for j in d:
-		if j in c:
-			flag=flag+1
-	if(flag==e):
+	for i in d:
+		if i in c:
+			count=count+1
+	if(count==e):
 		print "YES"
 	else:
 		print "NO"
